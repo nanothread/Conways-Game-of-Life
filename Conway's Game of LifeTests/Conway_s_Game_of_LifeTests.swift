d6b04d15f9718a73c0c960dc865b2bd2e740f920
Andrew Glen <andrew.r.glen@gmail.com>
2018-08-18 15:13:12 +0100

    Initial Commit

---
+++ Conway's Game of LifeTests/Conway_s_Game_of_LifeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Conway_s_Game_of_LifeTests.swift
+//  Conway's Game of LifeTests
+//
+//  Created by Andrew Glen on 18/08/2018.
+//  Copyright © 2018 LivePerson. All rights reserved.
+//
+
+import XCTest
+@testable import Conway_s_Game_of_Life
+
+class Conway_s_Game_of_LifeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
